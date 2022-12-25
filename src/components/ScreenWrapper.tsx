b8469f074e27a8d917a90f5a5051b6b02885a36d
juandsoto <juandsoto1006@gmail.com>
2022-12-25 11:39:25 -0500

    styles: add welcome page

--- src/components/ScreenWrapper.tsx
+++ src/components/ScreenWrapper.tsx
@@ -1,16 +1,17 @@
 import { AnimatePresence, motion } from "framer-motion";
 import { FADE_VARIANTS } from "utils/motion";
+import { HTMLAttributes } from "react";
 
-interface Props {
+interface Props extends HTMLAttributes<HTMLDivElement> {
   children: JSX.Element | JSX.Element[];
 }
 
-const ScreenWrapper = ({ children }: Props): JSX.Element => {
+const ScreenWrapper = ({ children, className, ...props }: Props): JSX.Element => {
   return (
     <AnimatePresence mode="wait">
-      <div className="layout h-screen w-screen overflow-hidden">
+      <div {...props} className="layout h-screen w-screen overflow-hidden">
         <motion.div
-          className="relative h-screen w-screen"
+          className={["relative h-screen w-screen", className].join(" ")}
           variants={FADE_VARIANTS}
           initial="hidden"
           animate="visible"
