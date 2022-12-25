4b1857e8678cdcb40d701129bde22522fc651a29
juandsoto <juandsoto1006@gmail.com>
2022-12-25 12:09:34 -0500

    styles: improvement

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -14,9 +14,9 @@ const Cell = ({ value, position }: Props): JSX.Element => {
     [OBJECTS.BLANK]: null,
     [OBJECTS.PLAYER]: <img className={["object car", canMovePlayer ? "" : "bg-red-500"].join(" ")} src="car.svg" />,
     [OBJECTS.GOAL]: <img className="object goal" src="goal.svg" />,
-    [OBJECTS.PERSON1]: <img className="object man py-2" src="person1.svg" />,
-    [OBJECTS.PERSON2]: <img className="object man" src="person2.svg" />,
-    [OBJECTS.CRACK]: <img className="object crack" src="crack.png" />,
+    [OBJECTS.PERSON1]: <img className="object man scale-75" src="person1.svg" />,
+    [OBJECTS.PERSON2]: <img className="object man py-1" src="person2.svg" />,
+    [OBJECTS.CRACK]: <img className="object crack" src="crack.svg" />,
   };
 
   return <div className={`cell position__${x}-${y}`}>{SPRITES[value]}</div>;
